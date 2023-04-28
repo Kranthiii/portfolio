208e05c8533b47fc4579c16c5057d677e62eb9c1
Kranthiii <lci2021053@iiitl.ac.in>
2023-04-29 00:57:54 +0530

    education page

--- src/pages/Bts.js
+++ src/pages/Bts.js
@@ -29,7 +29,7 @@ class Bts extends Component {
                                 }),
                             }}
                         >
-                            BTS SIO
+                            My Education
                         </TextAnimate>
                     </h2>
                     <div>
@@ -39,7 +39,7 @@ class Bts extends Component {
                             style = {{maxWidth: 900}}
                         >
                             <motion.p variants={item} style={{fontSize : "1.2em", paddingBottom : 10}}>
-                                Le BTS SIO est un diplôme d'État de niveau Bac+2
+                                {/* Le BTS SIO est un diplôme d'État de niveau Bac+2
                                 qui forme des professionnels de l'informatique
                                 capables de répondre aux besoins des
                                 organisations en matière de développement, de
@@ -48,32 +48,35 @@ class Bts extends Component {
                                 organisé autour de deux spécialités : SISR
                                 (Solutions d'Infrastructure, Systèmes et
                                 Réseaux) et SLAM (Solutions Logicielles et
-                                Applications Métiers).
+                                Applications Métiers). */}
+                                I am currently pursuing a four-year undergraduate degree in Computer Science & Artificial Intelligence from Indian Institute of Information Technology, Lucknow. I have completed courses on Data Structures, Analysis of Algorithms, Java, and other computer science-related topics. My current CGPA is 7.57/10.
                             </motion.p>
-                            <motion.h2 variants={item} style={{fontSize : "1.4em", padding : "10px 0"}}>L'option SISR</motion.h2>
+                            <motion.h2 variants={item} style={{fontSize : "1.4em", padding : "10px 0"}}>Intermediate</motion.h2>
                             <motion.p variants={item} style={{fontSize : "1.2em", paddingBottom : 10}}>
-                                La spécialité SISR forme des experts en systèmes
+                                {/* La spécialité SISR forme des experts en systèmes
                                 d'information et réseaux qui sont capables de
                                 mettre en place, configurer et maintenir les
                                 infrastructures informatiques d'une
                                 organisation. Ils sont également en mesure de
                                 résoudre les problèmes de sécurité informatique
-                                et de gérer les services réseaux.
+                                et de gérer les services réseaux. */}
+                                I have completed my intermediate education from Sri Chaitanya Junior Kalasala Hyderabad, Telangana. During this period, I studied Physics, Maths, and Chemistry, and achieved an impressive 98.2% percentage.
                             </motion.p>
-                            <motion.h2 variants={item} style={{fontSize : "1.4em", padding : "10px 0"}}>L'option SLAM</motion.h2>
+                            <motion.h2 variants={item} style={{fontSize : "1.4em", padding : "10px 0"}}>School</motion.h2>
                             <motion.p variants={item} style={{fontSize : "1.2em", paddingBottom : 10}}>
-                                La spécialité SLAM quant à elle, forme des
+                                {/* La spécialité SLAM quant à elle, forme des
                                 développeurs d'applications capables de
                                 concevoir, développer, tester et déployer des
                                 solutions logicielles pour répondre aux besoins
                                 métiers d'une organisation. Ils sont également
                                 capables de mettre en place des bases de données
                                 et de gérer l'ensemble du cycle de vie des
-                                applications.
+                                applications. */}
+                                I completed my high school education from Nobel High School Kapra Hyderabad, Telangana. During my high school education, I scored a perfect 10 GPA in my 10th grade. I was also the Head Girl of the institution.
                             </motion.p>
-                            <motion.h2 variants={item} style={{fontSize : "1.4em", padding : "10px 0"}}>Après le BTS SIO</motion.h2>
+                            {/* <motion.h2 variants={item} style={{fontSize : "1.4em", padding : "10px 0"}}>Après le BTS SIO</motion.h2> */}
                             <motion.p variants={item} style={{fontSize : "1.2em", paddingBottom : 20}}>
-                                Les étudiants du BTS SIO ont la possibilité de
+                                {/* Les étudiants du BTS SIO ont la possibilité de
                                 poursuivre leurs études après l'obtention de
                                 leur diplôme, en intégrant une licence
                                 professionnelle ou une école d'ingénieurs. Ils
@@ -81,17 +84,17 @@ class Bts extends Component {
                                 du travail en occupant des postes tels que
                                 technicien réseau, administrateur système,
                                 développeur d'applications, chef de projet
-                                informatique, etc.
+                                informatique, etc. */}
                             </motion.p>
                             <motion.p variants={item} style={{fontSize : "1.2em", paddingBottom : 20}}>
-                                En résumé, le BTS SIO est une formation
+                                {/* En résumé, le BTS SIO est une formation
                                 professionnalisante qui permet aux étudiants de
                                 développer des compétences techniques pointues
                                 dans les domaines de l'infrastructure, des
                                 réseaux et de l'applicatif. Cette formation
                                 répond aux besoins du marché de l'emploi dans le
                                 secteur de l'informatique, qui offre de
-                                nombreuses opportunités de carrière.
+                                nombreuses opportunités de carrière. */}
                             </motion.p>
                         </ContainerAnimate>
                     </div>
